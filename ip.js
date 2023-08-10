be2ffd317a129c363598450c95e82f4eeeb9fc6b
T0ne1d <141898642+T0ne1d@users.noreply.github.com>
2023-08-10 15:41:08 +0800

    -

--- ip.js
+++ ip.js
@@ -1,8 +1,27 @@
-$httpClient.get('https://api.my-ip.io/ip', function (error, response, data) {
-  $done({
-    title: 'Global IP',
-    content: data,
-    backgroundColor: '#663399',
-    icon: 'network',
-  })
-})
+let url = "http://ip-api.com/json"
+
+$httpClient.get(url, function(error, response, data){
+    let jsonData = JSON.parse(data)
+    let country = jsonData.country
+    let emoji = getFlagEmoji(jsonData.countryCode)
+    let city = jsonData.city
+    let isp = jsonData.isp
+    let ip = jsonData.query
+  body = {
+    title: "Global IP",
+    content: `${ip} | IP \n${isp} | ISP\n${emoji}${country} - ${city}`,
+    icon: "globe.asia.australia.fill"
+  }
+  $done(body);
+});
+
+function getFlagEmoji(countryCode) {
+  if (countryCode.toUpperCase() == 'TW') {
+    countryCode = 'CN'
+  }
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char => 127397 + char.charCodeAt())
+  return String.fromCodePoint(...codePoints)
+}
